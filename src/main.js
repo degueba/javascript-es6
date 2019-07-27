6aece280048d451bf6c55e1944c422f16d3a3a40
Rodrigo Botelho <rodrigo.botelho@original.io>
2019-07-27 02:27:27 -0300

    rocketseat complete course javascriptes6 concepts

---
+++ src/main.js
@@ -0,0 +1,87 @@
+import api from './api';
+
+
+class App {
+    constructor(){
+        this.repositories = [];
+
+        this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository]');
+        this.listEl = document.getElementById('repo-list');
+
+        this.bindEvents();
+    }
+
+
+    bindEvents(){
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    async addRepository(event){
+        event.preventDefault();
+
+        const repoInput = this.inputEl.value;
+        
+        if(repoInput.length === 0) return;
+
+        try {
+            console.log(repoInput);
+            
+            const response = await api.get(`/repos/${repoInput}`);
+            const {name, description, owner: {avatar_url}, html_url} = response.data;
+            
+            console.log(response.data);
+
+            this.repositories.push({
+                name,
+                description,
+                html_url,
+                avatar_url
+            });
+        } catch(err) {
+            console.warn('Ero na api -> ', err);
+        }
+        
+
+        
+
+        
+
+        this.render();
+    }
+
+    render(){
+        this.listEl.innerHTML = '';
+
+
+        // só percorre.. o map não, o map retorna algum item
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.setAttribute('href', repo.html_url);
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+
+        });
+    }
+}
+
+
+new App();
